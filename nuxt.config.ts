e18fc26cfa234fec31ed9cf45cd29e603e9eec63
IwuchukwuDivine <ekeneifunanya@gmail.com>
2024-06-12 20:35:00 +0100

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,22 @@
 import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
-  //...
   components: true,
-	ssr: false,
-	typescript: { shim: false },
+  ssr: false,
+  typescript: { shim: false },
   build: {
     transpile: ['vuetify'],
   },
-  modules: [
-   
-  ],
+  modules: [],
   vite: {
-		ssr: {
-			noExternal: ["vuetify"], // add the vuetify vite plugin
-		},
-	},
+    ssr: {
+      noExternal: ["vuetify"], // add the vuetify vite plugin
+    },
+  },
   css: [
-		"vuetify/lib/styles/main.sass",
-		"@mdi/font/css/materialdesignicons.min.css",
-	],
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  generate: {
+    fallback: true, // if you are using a SPA fallback, set this to true
+  }
 })
-
